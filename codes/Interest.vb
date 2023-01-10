d353fa1fce812cebb55505e63cbdb8cf24b5b817
NatthakanRitmak <122331833+NatthakanRitmak@users.noreply.github.com>
2023-01-09 18:56:38 -0800

    Add files via upload

---
+++ codes/Interest.vb
@@ -0,0 +1,17 @@
+﻿Imports System
+Module Interest
+    Public deposit, rate As Double
+    Sub Main()
+        Dim Year1, Year2 As Double
+        rate = 0.02
+        deposit = 10000
+
+        Year1 = deposit * (1 + rate)
+        REM Console.WriteLine("First year = {0}", year1)
+        Console.WriteLine("First year = {0}", Year1)
+        Year2 = Year1 * (1 + rate)
+        Console.WriteLine('Second year = {0:C}', Year2)
+        Console.ReadLine()
+    End Sub
+
+End Module
